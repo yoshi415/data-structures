6b166e91cb797e6fc058f8416f824503972798cc
MKS-Student <mks.student@hackreactor.com>
2015-06-10 15:04:51 -0700

    Finished stack & queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,32 @@ var Queue = function(){
   var storage = {};
 
   // Implement the methods below
-
+  var end = 0; // 5
+  var start = 1; // 6
   someInstance.enqueue = function(value){
+    // adds 1 to end & start
+    end++;
+    start++;
+    // stores value at current number of end
+    storage[end] = value;
   };
 
   someInstance.dequeue = function(){
+    if (end >= 1) {
+      var temp = storage[start - end]
+      delete storage[start - end]
+      start++;
+      
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    var count = 0;
+    for (var key in storage) {
+      count++
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,24 @@ var Stack = function(){
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
   someInstance.push = function(value){
+    size++;
+    storage[size] = value;
+
   };
 
   someInstance.pop = function(){
+    if (size >= 1) {
+      var temp = storage[size];
+      delete storage[size];
+      size--;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
