7a10acea464e92addd9d592e6658f097f31db03e
MKS-Student <mks.student@hackreactor.com>
2015-06-10 19:54:29 -0700

    Worked on stack prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,7 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stacker = Object.create(Stack.stuff);
+  var stacker = Object.create(stackMethods);
   stacker.size = 0;
   return stacker;
 };
@@ -9,11 +9,17 @@ var Stack = function() {
 var stackMethods = {};
 
 stackMethods.push = function(val) {
-
+  this.size++;
+  this[this.size] = val;
 }
 
 stackMethods.pop = function() {
-
+  if(this.size >= 1){
+    var temp = this[this.size];
+    delete this[this.size];
+    this.size--;
+    return temp;
+  }
 }
 stackMethods.size = function() {
   return this.size;
