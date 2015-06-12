b2fe16bb4933939636954f0752126c5685a5b87a
MKS-Student <mks.student@hackreactor.com>
2015-06-12 16:43:23 -0700

    started on graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,27 @@
 
 
 var Graph = function(){
+  this.storage = {};
 };
 
 Graph.prototype.addNode = function(node){
+  var newNode = new GraphNode(node);
+  this.storage[node] = newNode;
 };
 
 Graph.prototype.contains = function(node){
+  if (node in this.storage){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 Graph.prototype.removeNode = function(node){
+  for (var node in this.storage) {
+    delete this.storage[node];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
@@ -28,5 +40,8 @@ Graph.prototype.forEachNode = function(cb){
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+var GraphNode = function(value){
+   this.value = value;
+   this.edges = [];
+ }
 
